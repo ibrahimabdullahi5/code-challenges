3b44cea9ae549cfa6604c4a2528611b30c168c9c
ibrahimabdullahi5 <ibrahimabdullahibare@DESKTOP-0DEOD38.localdomain>
2024-05-15 20:54:57 -0900

    done studentgrade

---
+++ studentgrade/index.js
@@ -0,0 +1,26 @@
+function generateStudentsGrade(marks){  
+if (marks > 79){
+return 'A'
+}else if(marks >= 60 && marks <=79){
+  return 'B'
+}else if(marks > 49 && marks <=59){
+    return 'C'
+}else if(marks >= 40 && marks <=49){
+    return 'D'
+}else if(marks < 40){
+    return 'E'
+}
+} 
+
+// console.log(grade)
+
+const studentMarks = prompt('Enter student marks(between 0 and 100):');
+if (!isNaN(studentMarks)&& studentMarks <=100 && studentMarks >=0){
+const grade = generateStudentsGrade(studentMarks)
+console.log(grade)
+
+}else {
+    console.log('invalid input,please input a number between 0 and 100')
+}
+
+
